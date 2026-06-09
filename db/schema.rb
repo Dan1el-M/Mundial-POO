ffc6cacaaaf7e116e8c30c89fb51e00b6525c31f
Rasec081 <gamboacesar379@gmail.com>
2026-06-08 19:37:19 -0600

    creacion de la clase grupo

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_06_06_180200) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_09_010559) do
   create_table "groups", force: :cascade do |t|
     t.string "letter", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["letter"], name: "index_groups_on_letter", unique: true
+  end
+
+  create_table "grupos", force: :cascade do |t|
+    t.string "letra", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["letra"], name: "index_grupos_on_letra", unique: true
   end
 
   create_table "matches", force: :cascade do |t|
