b8545d97ca28aed5812fd11207adb5030b3d39b2
ldrichmondg <luis.richmondg@gmail.com>
2026-06-14 20:27:41 -0600

    Eliminacion de archivos en ingles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  root "posiciones#index"
+  root "selecciones#index"
 
   resources :selecciones, except: :show
   resources :grupos
